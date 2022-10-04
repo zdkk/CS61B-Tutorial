5a5e5c260dee4b6a8f8024557fca41861f975fec
zdkk <1040893382@qq.com>
2022-10-04 21:03:49 +0800

    project1 ec v1

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,16 @@
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+/**
+ * @author zdkk
+ * @create 2022-10-04 20:40
+ */
+public class TestArrayDequeEC {
+    @Test
+    public void test1() {
+        
+    }
+}
