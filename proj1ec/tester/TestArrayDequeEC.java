4bf73d6bf5d8955391b1945867ce122bc4bc10bd
zdkk <1040893382@qq.com>
2022-10-05 19:26:19 +0800

    project1 ec v2

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -3,6 +3,7 @@ package tester;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import student.StudentArrayDeque;
+import edu.princeton.cs.introcs.StdRandom;
 
 /**
  * @author zdkk
@@ -11,6 +12,31 @@ import student.StudentArrayDeque;
 public class TestArrayDequeEC {
     @Test
     public void test1() {
-        
+        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
+        for (int i = 0; i < 100; i++) {
+            int x = StdRandom.uniform(0, 3);
+            if (x == 0) {
+                a.addFirst(i);
+                b.addFirst(i);
+                System.out.println("addFirst(" + i + ")");
+                assertEquals("addFirst(" + i + ")", a.get(0), b.get(0));
+            } else if (x == 1){
+                a.addLast(i);
+                b.addLast(i);
+                System.out.println("addLast(" + i + ")");
+                assertEquals("addLast(" + i + ")", a.get(a.size() - 1), b.get(b.size() - 1));
+            } else if (x == 2 && a.size() > 0) {
+                Integer p = a.removeFirst();
+                Integer q = b.removeFirst();
+                System.out.println("removeFirst()");
+                assertEquals("removeFirst()", p, q);
+            } else if (x == 3 && a.size() > 0) {
+                Integer p = a.removeLast();
+                Integer q = b.removeLast();
+                System.out.println("removeLast()");
+                assertEquals("removeLast()", p, q);
+            }
+        }
     }
 }
